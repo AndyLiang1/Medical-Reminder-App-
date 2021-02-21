cca2e9c876e8be427e526a94de2bdac364b8e064
Ha Thu Nguyen <hathun@sfu.ca>
2021-02-20 16:45:07 -0800

    Set up basic bottom navigation bar.

--- app/src/main/java/stormhacks2021/MedicationReminderApp/MainActivity.java
+++ app/src/main/java/stormhacks2021/MedicationReminderApp/MainActivity.java
@@ -1,8 +1,13 @@
 package stormhacks2021.MedicationReminderApp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupNavigationBar();
+    }
+
+    private void setupNavigationBar() {
+        BottomNavigationView bottomNavigationBar = (BottomNavigationView) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.bottom_navigation_homepage:
+                        selectedFragment = new HomeFragment();
+                        break;
+
+                    case R.id.bottom_navigation_reminder:
+                        selectedFragment = new ReminderFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 }
