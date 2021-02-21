3c78f8c190dc38725ecbb5dec4d7b2cf11a1e7e5
mwZ101 <marvinvwu@gmail.com>
2021-02-21 00:32:04 -0800

    Added some code for what to do when saving a reminder that's being inserted

--- app/src/main/java/stormhacks2021/MedicationReminderApp/HomeFragment.java
+++ app/src/main/java/stormhacks2021/MedicationReminderApp/HomeFragment.java
@@ -1,4 +1,5 @@
 package stormhacks2021.MedicationReminderApp;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Activity activity = getActivity();
 
         Button insertButton = (Button) view.findViewById(R.id.insertReminderButton);
 
@@ -19,6 +21,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "Creating a new reminder...", Toast.LENGTH_SHORT).show();
+                Intent insertIntent = new Intent(getActivity(), InsertActivity.class);
+                activity.startActivity(insertIntent);
+                activity.finish();
             }
         });
 
